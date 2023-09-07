5489725ee6fa15ab83aafc1657464a4b1392e657
Kevin Jake <kevin_jake_04@yahoo.com>
2023-09-07 11:21:15 +0800

    add pretty print

--- generic_shopify_scraper/scrape_shopify.py
+++ generic_shopify_scraper/scrape_shopify.py
@@ -29,24 +29,30 @@ product_items = []
 def parse_product(result):
     for item in result:
         product_item = {}
-        print('----------------------------------------------------')
-        print(item)
-        print('----------------------------------------------------')
-        product_item['url'] =  url + "/products/" + item['handle']
-        product_item['name'] = item['title']
-        product_item['price'] = item['variants'][0]['price']
-        product_item['brand'] = item['vendor']
-        product_item['supplier'] = config['supplier']
-        promo_price = item['variants'][0]['compare_at_price']
-        if promo_price:
-            product_item['promo'] = 'Save ' + str(float(promo_price) - float(product_item['price']))
-        # TODO: Find way to scrape inventory number in shopify
-        # product_item['warranty'] = response.css('div.MuiBox-root.css-i2n2aa div.MuiBox-root.css-w55c3f p::text').get().strip()
-        # product_item['stocks'] = item['stocks_left']
-        print('----------------------------------------------------')
-        print(product_item)
-        print('----------------------------------------------------')
-        product_items.append(product_item)
+        # print('----------------------------------------------------')
+        # print(item)
+        # print('----------------------------------------------------')
+        if item['variants'][0]['available']:
+            product_item['url'] =  url + "/products/" + item['handle']
+            product_item['name'] = item['title']
+            product_item['price'] = item['variants'][0]['price']
+            product_item['brand'] = item['vendor']
+            product_item['supplier'] = config['supplier']
+            promo_price = item['variants'][0]['compare_at_price']
+
+            if promo_price:
+                compare_price = float(promo_price) - float(product_item['price'])
+                if compare_price > 0:
+                    product_item['promo'] = 'Save ' + str(compare_price)
+
+            # TODO: Find way to scrape inventory number in shopify
+            # product_item['warranty'] = response.css('div.MuiBox-root.css-i2n2aa div.MuiBox-root.css-w55c3f p::text').get().strip()
+            # product_item['stocks'] = item['stocks_left']
+
+            print('----------------------------------------------------')
+            print(product_item)
+            print('----------------------------------------------------')
+            product_items.append(product_item)
     return product_items
 
  
@@ -59,7 +65,7 @@ if __name__ == "__main__":
         parse_product(result)
         page += 1
 
-    jsonString = json.dumps(product_items)
+    jsonString = json.dumps(product_items, indent=2, separators=(',', ': '),)
     jsonFile = open(f'{config["filename_prefix"]}_{args.product}.json', "w")
     jsonFile.write(jsonString)
     jsonFile.close()
